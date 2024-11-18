977369419abe27d3e06e9cacbbe427d1b5ddb5cd
Hans-Christian Knolle <hanscknolle08@gmail.com>
2024-11-18 16:02:28 +0100

    Third Learning Project: About Me Generator

--- src/LearningProjects/AboutMeGenerator.py
+++ src/LearningProjects/AboutMeGenerator.py
@@ -1,8 +1,8 @@
 print("Hallo :D Willkommen zu diesem Generator\n")
 name = str(input("Nenne mir zuerst deinen Namen:\n"))
-home = str(input("Sag uns wo du wohnst:\n"))
+straße = str(input("Sag uns wo du wohnst:\n"))
 age = int(input("Jetzt dein Alter:\n"))
 birthdate = str(input("Jetzt brauche ich deinen Geburtstag:\n"))
 desc = str(input("Beschreib dich:\n"))
 
-print(f'Hallo!, mein Name ist {name} und ich wohne in {home}')
+print(f'\nHallo!, mein Name ist {name} und ich wohne in der {straße}.\n Ich bin {age} alt und wurde am {birthdate} geboren.\n So bin ich:\n {desc}')
