a4d63cd6f4434371e503c0b9c290d8031089de58
Hans-Christian Knolle <hanscknolle08@gmail.com>
2024-11-18 14:25:12 +0100

    Starting new Learning Project: About me generator

---
+++ src/LearningProjects/AboutMeGenerator.py
@@ -0,0 +1,4 @@
+print("Hallo :D Willkommen zu diesem Generator\n")
+name = str(input("Nenne mir zuerst deinen Namen:\n"))
+age = int(input("Jetzt dein Alter:\n"))
+birthdate = str(input("Jetzt brauche ich deinen Geburtstag:\n"))
