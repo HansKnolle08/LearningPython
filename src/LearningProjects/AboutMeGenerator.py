dae215ec2f147de6d7650d27603739c00b0a8e31
Hans-Christian Knolle <hanscknolle08@gmail.com>
2024-11-18 15:18:12 +0100

    Progress in About me gen

--- src/LearningProjects/AboutMeGenerator.py
+++ src/LearningProjects/AboutMeGenerator.py
@@ -1,4 +1,8 @@
 print("Hallo :D Willkommen zu diesem Generator\n")
 name = str(input("Nenne mir zuerst deinen Namen:\n"))
+home = str(input("Sag uns wo du wohnst:\n"))
 age = int(input("Jetzt dein Alter:\n"))
 birthdate = str(input("Jetzt brauche ich deinen Geburtstag:\n"))
+desc = str(input("Beschreib dich:\n"))
+
+print(f'Hallo!, mein Name ist {name} und ich wohne in {home}')
