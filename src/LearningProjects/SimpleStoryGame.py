1ae924d7abd2ee37f9526bda5ef23b97ecc7ebb8
Hans-Christian Knolle <hanscknolle08@gmail.com>
2024-11-19 10:34:45 +0100

    Finished Control Structur, added error handling and started with storytelling

--- src/LearningProjects/SimpleStoryGame.py
+++ src/LearningProjects/SimpleStoryGame.py
@@ -3,13 +3,29 @@ from time import sleep
 print("Willkommen zu diesem kleinen Story Game!")
 print("Tauche mit ein in die atemberaunde Geschichte dieses Spiels\n")
 
+name: str = str(input("Nenne mir deinen Namen:\n"))
+
 sleep(1)
 
 start_game_variations: list = ["start", "Start", "start game", "Start Game", "startgame"]
 quit_game_variations: list = ["quit", "Quit", "quit game", "Quit Game", "quitgame"]
-choice: int = None
+choice: str = None
 
-def game(name: str) -> None:
+def section1(choice: str) -> None:
+    if choice == "1":
+        print(choice)
+    elif choice == "2":
+        print(choice)
+    elif choice == "3":
+        print(choice)
+    else:
+        try:
+            section1(choice = str(input(f"Was soll {name} machen?:\n")))
+        except ValueError:
+            print("Bitte wähle eine geltende eingabe!")
+            section1(choice = str(input(f"Was soll {name} machen?:\n")))
+
+def game() -> None:
     print(f'Es war ein schöner Tag und {name} machte einen Spaziergang durch den Wald')
     sleep(4)
     print(f'Plötzlich hörte {name} eine Sirene!')
@@ -28,22 +44,18 @@ def game(name: str) -> None:
     print("1. Weglaufen")
     print("2. Kämpfen")
     print("3. Mit Sirenhead reden")
-    choice = int(input(f"Was soll {name} machen?:\n"))
-    if choice == 1:
-        print(choice)
-    elif choice == 2:
-        print(choice)
-    elif choice == 3:
-        print(choice)
-    else:
-        print(choice)
+    try:
+        section1(choice = str(input(f"Was soll {name} machen?:\n")))
+    except ValueError:
+        print("Bitte wähle eine geltende eingabe!")
+        section1(choice = str(input(f"Was soll {name} machen?:\n")))
 
 def main() -> None:
     print("### Start Game ###")
     print("### Quit Game ###\n")
     menu_choice: str = str(input("Wähle aus was du machen möchtest:\n"))
     if menu_choice in start_game_variations:
-        game(name = str(input("Sag mir deinen Namen:\n")))
+        game()
     elif menu_choice in quit_game_variations:
         print("Verlasse Spiel...")
         sleep(1)
